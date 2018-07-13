3dcaa2ab4aac75b7fc44ebf892df818c30b44698
PK <sirberus.io@gmail.com>
2018-07-13 00:02:25 -0700

    renamed

--- src/vue-plugin.js
+++ src/vue-plugin.js
@@ -3,7 +3,7 @@ const components = require('./components.js')
 module.exports = {
   install: (Vue, options) => {
     Vue.prototype.$F = options.feathersClient
-    Vue.component('feathers-observable', components.FeathersObservable)
-    Vue.component('feathers-stream', components.FeathersObservableStream)
+    Vue.component('observable-object', components.ObservableObject)
+    Vue.component('observable-stream', components.ObservableStream)
   },
 }
