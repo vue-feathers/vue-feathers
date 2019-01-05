0b5494e5cbb5f0740650dc7588901be7dcf6e599
Patrick Keenan <sirberus.io@gmail.com>
2019-01-05 01:35:32 -0800

    0.2.2 bugfix

--- src/vue-plugin.js
+++ src/vue-plugin.js
@@ -2,5 +2,6 @@ import components from './components.js'
 
 export default (Vue, options) => {
   Vue.prototype.$F = options.feathersClient
-  Vue.component('feathers-stream', components.FeathersStream)
+  Vue.component('observable-object', components.ObservableObject)
+  Vue.component('observable-stream', components.ObservableStream)
 }
