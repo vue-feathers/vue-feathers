ba11be738663751b55fd1c1227d49dca63a63360
Patrick Keenan <sirberus.io@gmail.com>
2019-01-12 22:28:43 -0800

    0.3.2

--- changelog.md
+++ changelog.md
@@ -1,5 +1,5 @@
-## v0.3.1 - 2019-01-11
-- Vue plugin fix
+## v0.3.1-2 - 2019-01-11
+- Vue plugin fixes
 - Readme updates
 
 ## v0.3.0 - 2019-01-11
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-export import plugin from './vue-plugin.js'
-export import mixins from './mixins.js'
+export { default as plugin } from './vue-plugin.js'
+export { default as mixins } from './mixins.js'
 
 export default {
   install: plugin,
