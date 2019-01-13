44de4df95e4699d63c571431355dbab328cb5d9b
Patrick Keenan <sirberus.io@gmail.com>
2019-01-12 19:44:10 -0800

    subscription handling

--- src/mixins.js
+++ src/mixins.js
@@ -31,29 +31,43 @@ const ListsMixin = (endpoints) => {
 const StreamsMixin = (endpoints) => {
   return {
     data() {
-      let obj = {}
+      let obj = {
+        subscriptions: {}
+      }
       for (let endpoint of endpoints) {
         obj[endpoint] = []
+        obj.subscriptions[endpoint] = null
       }
       return obj
     },
     methods: {
       sub(endpoint) {
-        this.$F.service(endpoint)
-          .watch({listStrategy: 'always'})
-          .find()
-          .subscribe(d => {
-            this[endpoint] = d
-          })
-      },
-      subAll() {
-        for (let endpoint of endpoints) {
+        this.unsub(endpoint) // unsub() before subbing in case component is re-mounted.
+        this.$set( // vm.$set() avoids Vue.js caveat on object update reactivity.
+          this.subscriptions,
+          endpoint,
           this.$F.service(endpoint)
             .watch({listStrategy: 'always'})
             .find()
             .subscribe(d => {
               this[endpoint] = d
             })
+        )
+      },
+      subAll() {
+        for (let endpoint of endpoints) {
+          this.sub(endpoint)
+        }
+      },
+      unsub(endpoint) {
+        if (this.subscriptions[endpoint]) {
+          this.subscriptions[endpoint].unsubscribe()
+          this.$set(this.subscriptions, endpoint, null)
+        }
+      },
+      unsubAll() {
+        for (let endpoint of endpoints) {
+          this.unsub(endpoint)
         }
       },
     },
